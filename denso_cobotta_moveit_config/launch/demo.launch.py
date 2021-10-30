14d04ddb20672c44eeac84318c7516055406a6f9
Floccari <nexy@tutanota.de>
2021-10-30 11:26:05 +0200

    rviz works better

--- denso_cobotta_moveit_config/launch/demo.launch.py
+++ denso_cobotta_moveit_config/launch/demo.launch.py
@@ -1,11 +1,13 @@
 import os
 import yaml
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import xacro
 
@@ -37,8 +39,8 @@ def generate_launch_description():
     # planning_context
     robot_description_config = xacro.process_file(
         os.path.join(
-            get_package_share_directory("denso_cobotta_descriptions"),
-            "cobotta_description",
+            get_package_share_directory("denso_cobotta_moveit_config"),
+            "config",
             "cobotta.urdf.xacro",
         ),
         mappings={'gripper_type'  : 'parallel'}
@@ -62,6 +64,39 @@ def generate_launch_description():
         "denso_cobotta_moveit_config", "config/kinematics.yaml"
     )
 
+    # Planning Functionality
+    ompl_planning_pipeline_config = {
+        "move_group": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+            "start_state_max_bounds_error": 0.1,
+        }
+    }
+
+    # Trajectory Execution Functionality
+    controllers_yaml = load_yaml(
+        "denso_cobotta_moveit_config",
+        "config/controllers.yaml"
+    )
+    moveit_controllers = {
+        "moveit_simple_controller_manager": controllers_yaml,
+        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
+    }
+
+    trajectory_execution = {
+        "moveit_manage_controllers": True,
+        "trajectory_execution.allowed_execution_duration_scaling": 1.2,
+        "trajectory_execution.allowed_goal_duration_margin": 0.5,
+        "trajectory_execution.allowed_start_tolerance": 0.01,
+    }
+
+    planning_scene_monitor_parameters = {
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+    }
+    
     # Start the actual move_group node/action server
     run_move_group_node = Node(
         package="moveit_ros_move_group",
@@ -70,7 +105,11 @@ def generate_launch_description():
         parameters=[
             robot_description,
             robot_description_semantic,
-            kinematics_yaml
+            kinematics_yaml,
+            ompl_planning_pipeline_config,
+            trajectory_execution,
+            moveit_controllers,
+            planning_scene_monitor_parameters,
         ],
     )
 
@@ -87,8 +126,24 @@ def generate_launch_description():
             robot_description,
             robot_description_semantic,
             kinematics_yaml,
+            ompl_planning_pipeline_config,
         ]
     )
+
+    # Gazebo
+    world_file = os.path.join(get_package_share_directory('denso_cobotta_gazebo'),
+                                'worlds', 'cobotta.world')
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+                launch_arguments={'world': world_file}.items(),
+             )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'cobotta'],
+                        output='screen')
 
     # Static TF
     static_tf = Node(
@@ -108,6 +163,34 @@ def generate_launch_description():
         parameters=[robot_description],
     )
 
+    # ros2_control
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("denso_cobotta_moveit_config"),
+        "config",
+        "ros_controllers.yaml",
+    )
+    
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, ros2_controllers_path],
+        output={
+            "stdout": "screen",
+            "stderr": "screen",
+        },
+    )
+
+    # Load controllers
+    load_controllers = []
+    for controller in ["cobotta_arm_controller", "joint_state_broadcaster"]:
+        load_controllers += [
+            ExecuteProcess(
+                cmd=["ros2 run controller_manager spawner.py {}".format(controller)],
+                shell=True,
+                output="screen",
+            )
+        ]
+
     # Warehouse mongodb server
     #mongodb_server_node = Node(
     #    package="warehouse_ros_mongo",
@@ -126,6 +209,10 @@ def generate_launch_description():
             static_tf,
             robot_state_publisher,
             run_move_group_node,
+            ros2_control_node,
             #mongodb_server_node,
+            #gazebo,
+            #spawn_entity,
         ]
+        + load_controllers
     )
